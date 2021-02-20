1b68a68a56b637f98de3ef74028ecd331bda4477
student <rahmafaizyusuf@gmail.com>
2021-02-20 22:57:48 +0300

    first commit

---
+++ src/student records.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var fullname=("rahmafaiz")
+    println(fullname)
+    var age=22
+    println(age)
+    var phonenumber="0797776371"
+    println(phonenumber)
+    var weightinkg=47
+    println(weightinkg)
+    var citizenship=("kenyan")
+    println(citizenship)
+}
